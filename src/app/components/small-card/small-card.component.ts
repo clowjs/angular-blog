3bc05a2f942d2625a1a03253c45abbdcb97f586e
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-01-25 10:14:06 -0300

    finished?

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input() hr: string = 'no';
+  @Input() img: string = '';
+  @Input() date: string = '';
+  @Input() title: string = '';
 
   constructor() { }
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  articles: any[] = [
+    {
+      img: 'https://miro.medium.com/v2/resize:fit:720/format:webp/0*DTy8tlNHXGc0g4sB',
+      date: 'March 20th, 2022',
+      title: 'Introducing Angular v17',
+    },
+    {
+      img: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*aCsnm9viiL5Fo0JC-I_exQ.png',
+      date: 'August 15th, 2023',
+      title: 'Announcing Angular.dev',
+    },
+    {
+      img: 'https://miro.medium.com/v2/resize:fit:720/format:webp/0*1yo17Ov28sa7p33N',
+      date: 'January 13th, 2024',
+      title: 'New free, interactive Angular course for beginners on Sololearn',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
